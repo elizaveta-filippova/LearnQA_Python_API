1edb50d0a3513162a887f28b845ad1f228799f5c
elizaveta-filippova <rustcohlehere@gmail.com>
2022-12-04 12:49:28 +0400

    ex 15

---
+++ tests/test_user_get.py
@@ -0,0 +1,56 @@
+import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+from lib.data_generator import DataGenerator
+
+
+class TestUserGet(BaseCase):
+    user_data = {
+        'email': 'vinkotov@example.com',
+        'password': '1234'
+    }
+
+    def setup(self):
+        self._data_generator = DataGenerator()
+        self.email = self._data_generator.email
+        self.username = self._data_generator.username
+
+    def test_get_user_details_not_auth(self):
+        response = requests.get("https://playground.learnqa.ru/api/user/2")
+
+        Assertions.assert_json_has_key(response, "username")
+        Assertions.assert_json_has_not_key(response, "email")
+        Assertions.assert_json_has_not_key(response, "firstName")
+        Assertions.assert_json_has_not_key(response, "lastName")
+
+    def test_get_user_details_auth_as_same_user(self):
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=self.user_data)
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+        response2 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id_from_auth_method}",
+                                 headers={'x-csrf-token': token}, cookies={'auth_sid': auth_sid})
+        expected_fields = ["username", "email", "firstName", "lastName"]
+
+        Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_user_details_auth_as_another_user(self):
+        new_user_data = self._data_generator.generate_data()
+        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=new_user_data) # создаем нового пользователя
+        new_user_id = self.get_json_value(response1, "id")
+
+        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=self.user_data) # логинимся под нашим пользователем
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        response3 = requests.get(f"https://playground.learnqa.ru/api/user/{new_user_id}",
+                                 headers={'x-csrf-token': token}, cookies={'auth_sid': auth_sid}) # пытаемся получить данные нового пользователя
+        expected_field = "username"
+        unexpected_fields = ["email", "firstName", "lastName"]
+
+        Assertions.assert_json_has_not_keys(response3, unexpected_fields)
+        Assertions.assert_json_has_key(response3, expected_field)
+
+
+
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -3,7 +3,7 @@ import requests
 import random
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
-from datetime import datetime
+from lib.data_generator import DataGenerator
 
 from lib.helpers import get_dict_wo_key, random_string
 
@@ -20,21 +20,12 @@ class TestUserRegister(BaseCase):
     ]
 
     def setup(self):
-        base_part = "learnqa"
-        domain = "example.com"
-        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        self.email = f"{base_part}{random_part}@{domain}"
-        self.username = "learnqa"
-
-    def generate_data(self, email=None, username=None):
-        keys = ['password', 'username', 'firstName', 'lastName', 'email']
-        values = ['123', f'{self.username if not username else username}', 'learnqa', 'learnqa',
-                  f'{self.email if not email else email}']
-
-        return {key: value for key, value in zip(keys, values)}
+        self._data_generator = DataGenerator()
+        self.email = self._data_generator.email
+        self.username = self._data_generator.username
 
     def test_create_user_successfully(self):
-        data = self.generate_data()
+        data = self._data_generator.generate_data()
 
         response = requests.post(url=self.url, data=data)
 
@@ -44,7 +35,7 @@ class TestUserRegister(BaseCase):
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
 
-        data = self.generate_data(email)
+        data = self._data_generator.generate_data(email)
         response = requests.post(url=self.url, data=data)
 
         Assertions.assert_code_status(response, 400)
@@ -52,7 +43,7 @@ class TestUserRegister(BaseCase):
 
     def test_create_user_with_email_without_at_symbol(self):
         email = self.email.replace("@", "")
-        data = self.generate_data(email)
+        data = self._data_generator.generate_data(email)
         response = requests.post(url=self.url, data=data)
 
         Assertions.assert_code_status(response, 400)
@@ -61,7 +52,7 @@ class TestUserRegister(BaseCase):
     @pytest.mark.parametrize('excluded_data', exclude_data)
     def test_create_user_with_missing_data(self, excluded_data):
         key_to_exclude = ''.join(list(map(' '.join, excluded_data)))  # достаем ключ в формате sting из tuple
-        data = get_dict_wo_key(self.generate_data(),
+        data = get_dict_wo_key(self._data_generator.generate_data(),
                                key_to_exclude)  # исключаем из сгенерированного словаря нужный ключ
         response = requests.post(self.url, data=data)
 
@@ -70,7 +61,7 @@ class TestUserRegister(BaseCase):
 
     def test_create_user_with_one_symbol_username(self):
         username = random_string(random.randint(1, 1))
-        data = self.generate_data(username=username)
+        data = self._data_generator.generate_data(username=username)
         response = requests.post(self.url, data=data)
 
         Assertions.assert_code_status(response, 400)
@@ -78,7 +69,7 @@ class TestUserRegister(BaseCase):
 
     def test_create_user_with_too_long_username(self):
         username = random_string(random.randint(251, 1000))
-        data = self.generate_data(username=username)
+        data = self._data_generator.generate_data(username=username)
         response = requests.post(self.url, data=data)
 
         Assertions.assert_code_status(response, 400)
